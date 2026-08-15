d94020665d46ef3575cbe98aded5510b3d6de156
Vyas Ramasubramani <vyasr@nvidia.com>
2026-08-15 15:52:07 -0700

    Fix pooled stream synchronization

--- cpp/src/io/utilities/datasource.cpp
+++ cpp/src/io/utilities/datasource.cpp
@@ -214,7 +214,7 @@ class device_buffer_source final : public datasource {
     auto const stream = cudf::detail::global_cuda_stream_pool().get_stream();
     auto h_data       = cudf::detail::make_host_vector_async(
       cudf::device_span<std::byte const>{_d_buffer.data() + offset, count}, stream);
-    stream.sync();
+    stream.synchronize();
     return std::make_unique<owning_buffer<cudf::detail::host_vector<std::byte>>>(std::move(h_data));
   }
 

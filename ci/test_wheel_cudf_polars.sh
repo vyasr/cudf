e718d5c9e77dcbd600abafd3e135e04d9711b107
Vyas Ramasubramani <vyasr@nvidia.com>
2026-09-21 14:53:35 -0700

    TEST: Report cudf-polars xdist timing

--- ci/test_wheel_cudf_polars.sh
+++ ci/test_wheel_cudf_polars.sh
@@ -108,6 +108,7 @@ for version in "${VERSIONS[@]}"; do
         "${COVERAGE_ARGS[@]}" \
         --numprocesses=4 \
         --dist=worksteal \
+        --engine-pool-timings \
         --durations=50 --durations-min=1 \
         -x \
         --junitxml="${RAPIDS_TESTS_DIR}/junit-cudf-polars-${version}.xml"
